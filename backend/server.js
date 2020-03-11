3d570aef94feacc810da99f83a735e284a972064
Siwire <kholyavo.ivan@gmail.com>
2020-03-12 02:00:06 +0300

    create upload folder

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
+const fs = require('fs');
+
 const cors = require('cors');
 
 app.use(cors());
@@ -11,5 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8000;
 require('./route')(app);
 app.listen(port, () => {
+  if (!fs.existsSync('upload')){
+    fs.mkdirSync('upload');
+  }
   console.log('We are live on ' + port);
 });
